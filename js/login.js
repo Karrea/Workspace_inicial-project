22bb82e9a3be5de8ee6d9b341b5fdcd6a3801ac4
Matias <carreamatias@gmail.com>
2020-08-12 16:45:31 -0300

    Redireccionar terminado, sin errores

--- js/login.js
+++ js/login.js
@@ -5,6 +5,14 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 });
 
+//Probando
+let user = document.getElementById("user");
+let pass = document.getElementById("pass")
+
 function toHome() {
-    location.href = "file:///C:/Users/Mati/Desktop/Cursos/JaP/fase%202/eMercado%20week%200/home.html"
+    if (user.value && pass.value) {
+        location.href = "file:///C:/Users/Mati/Desktop/Cursos/JaP/fase%202/eMercado%20week%200/home.html";
+    } else {
+        alert("Debes ingresar tus credenciales");
+    }
 }
