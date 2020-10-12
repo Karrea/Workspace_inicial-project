e54c571d2a6653a6c64f592fabf7973c7d55405b
Matias <carreamatias@gmail.com>
2020-10-11 21:03:05 -0300

    Carrito estatico agregado
    Agregué el carrito con valores fijos, proximamente agregar funciones que funcionen en tiemnpo real

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,139 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var articlesArray = [];
+var subtotal = 0;
+var unitSubtotal = subtotal;
+var envio;
+var total;
 
-});
+//Funcion que no permite seleccionar menos que 0 articulos
+function greatherThan() {
+    
+    var input = document.querySelectorAll('td > input')
+    
+    if ( input.value < 0) {
+        input.value = 0;
+    }
+}
+
+//Funcion que convierte a usd el subtotal de cada articulo para sumarlo en el total
+function toDolar(article) {
+    
+    if (article.currency == 'UYU') {
+        var usdPrice = article.unitCost / 40;
+        unitSubtotal= usdPrice * article.count;
+    } else {
+        unitSubtotal= article.unitCost * article.count;
+    }
+    subtotal += unitSubtotal;
+}
+
+//funcion que calcula los precios
+function priceCalculator() {
+
+    total = subtotal + envio;
+
+}
+
+function calcularEnvio() {
+    var premiumValue = document.getElementById('premiumRadio').checked;
+    var expressValue = document.getElementById('expressRadio').checked;
+    var estandarValue = document.getElementById('estandarRadio').checked
+
+    if (premiumValue) {
+        envio = (subtotal * 0.15);
+    } else if (expressValue) {
+        envio = (subtotal * 0.07);
+    } else if (estandarValue) {
+        envio = (subtotal * 0.03);
+    }
+}
+
+function showArticles() {
+    const tableContainer = document.getElementById("tableBody");
+    let toAppend = ``;
+
+    for (article of articlesArray) {
+
+        toDolar(article);
+        
+
+        toAppend += `
+            <tr> 
+              <td class='w-50'> <img src=' ${article.src}' class='w-25' > ${article.name}  </th>
+              <td> $ ${article.unitCost} ${article.currency} </td>
+              <td> <input id='cantidadEntrada' type='number' class='w-25' min='0' onkeyup='greatherThan()' value='${article.count}'> </th>
+              <td id='subtotal'> ${unitSubtotal} </th>
+            </tr>
+          
+        
+        `; 
+    }
+
+    tableContainer.innerHTML = toAppend;
+}
+
+function showCostos() {
+    var toAppend = `
+    
+    <h4 class="mb-3">Costos</h4>
+    <ul class="list-group mb-3">
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">Subtotal</h6>
+                    <small class="text-muted">Unitario del producto</small>
+                  </div>
+                  <span class="text-muted" id="productCostText"> ${subtotal} </span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">Envio</h6>
+                    <small class="text-muted">Según el tipo de publicación</small>
+                  </div>
+                  <span class="text-muted" id="comissionText"> ${envio} </span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>Total ($)</span>
+                  <strong id="totalCostText"> ${total} </strong>
+                </li>
+              </ul>
+            <hr class="mb-4">
+    `;
+    const divCostos = document.getElementById('costos');
+
+    divCostos.innerHTML += toAppend;
+}
+
+document.addEventListener('DOMContentLoaded', function(e) {
+
+    //Función que se ejecuta una vez que se haya lanzado el evento de
+    //que el documento se encuentra cargado, es decir, se encuentran todos los
+    //elementos HTML presentes.
+    getJSONData(CART_INFO_URL).then(function(resultObj) { 
+        if (resultObj.status === 'ok') {
+
+            //iguala el json al array vacio que declaramos al comienzo
+            articlesArray = resultObj.data.articles;
+            
+            //llama la funcion que muestra los articulos
+            showArticles();
+            //llama la funcion que calcula el precio
+            calcularEnvio()
+            
+            priceCalculator();
+            
+            showCostos();
+            
+        }
+            
+    })
+
+    
+})
+
+
+
+
+
+
+
+
+
--- js/init.js
+++ js/init.js
@@ -1,65 +1,63 @@
-const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
-const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
-const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
-const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
-const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
-const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CATEGORIES_URL ="https://japdevdep.github.io/ecommerce-api/category/all.json";
+const PUBLISH_PRODUCT_URL ="https://japdevdep.github.io/ecommerce-api/product/publish.json";
+const CATEGORY_INFO_URL ="https://japdevdep.github.io/ecommerce-api/category/1234.json";
+const PRODUCTS_URL ="https://japdevdep.github.io/ecommerce-api/product/all.json";
+const PRODUCT_INFO_URL ="https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const PRODUCT_INFO_COMMENTS_URL ="https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
-}
+};
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
-}
+};
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
+    .then((response) => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then((response) => {
+      result.status = "ok";
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = "error";
+      result.data = error;
+      hideSpinner();
+      return result;
     });
-}
+};
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
- 
+document.addEventListener("DOMContentLoaded", function (e) {
   showName();
 });
 
 function showName() {
-  const nav = document.getElementById('navigatorBar');
+  const nav = document.getElementById("navigatorBar");
   let toAppend = `
     <a class="py-2 d-none d-md-inline-block" href="home.html">Inicio</a>
     <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
     <a class="py-2 d-none d-md-inline-block" href="products.html">Productos</a>
     <a class="py-2 d-none d-md-inline-block" href="sell.html">Vender</a>
 
-    <div class="nav-item dropdown ">
+    <div class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="perfilButton" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        ${ localStorage.getItem('mail') } 
+        ${localStorage.getItem("mail")} 
       </a>
       <div class="dropdown-menu" aria-labelledby="perfilButton">
         <a class="dropdown-item" href="cart.html"> Mi carrito </a>
@@ -68,12 +66,13 @@ function showName() {
         <a class="dropdown-item" onClick='deleteSession()'> Salir </a>
       </div>
     </div>
-  `; // 
+  `; //
 
   nav.innerHTML = toAppend;
 }
 
+// Funcion que deletea la sesion y vuelve al login
 function deleteSession() {
-  localStorage.clear()
+  localStorage.clear();
   location.href = "index.html";
 }
